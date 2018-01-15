fd4e9f216c2e6c1a9f34fbf7cf6282651a396e30
sante.stanisci@masvis.com <sante.stanisci@masvis.com>
2018-01-15 14:59:43 +0100

    update relation methods

--- src/resource.ts
+++ src/resource.ts
@@ -27,42 +27,37 @@ export abstract class Resource {
     }
 
     // Get collection of related resources
-
-    public getAll<T extends Resource>(type: { new(): T }, relation: string, options?: {
+    public getRelationArray<T extends Resource>(type: { new(): T }, relation: string, options?: {
         size?: number, sort?: Sort[],
         params?: [{ key: string, value: string | number }]
-    }): Observable<ResourceArray<T>> {
+    }): Observable<T[]> {
 
         const params = ResourceHelper.optionParams(new HttpParams(), options);
         const result: ResourceArray<T> = ResourceHelper.createEmptyResult<T>(this.http);
         result.observable = this.http.get(this.getURL(this._links[relation].href), {headers: ResourceHelper.headers, params: params});
-        return result.observable.map(response => ResourceHelper.instantiateResourceCollection<T>(type, response, result));
+        return result.observable.map(response => ResourceHelper.instantiateResourceCollection<T>(type, response, result))
+            .map((array: ResourceArray<T>) => array.result);
     }
 
     // Get related resource
-
-    public get<T extends Resource>(type: { new(): T }, relation: string): Observable<T> {
+    public getRelation<T extends Resource>(type: { new(): T }, relation: string): Observable<T> {
         const result: T = new type();
         result.observable = this.http.get(this.getURL(this._links.relation.href), {headers: ResourceHelper.headers});
         return result.observable.map(data => ResourceHelper.instantiateResource(result, data, this.http));
     }
 
     // Bind the given resource to this resource by the given relation
-
-    public bind<T extends Resource>(resource: T): Observable<any> {
+    public updateRelation<T extends Resource>(resource: T): Observable<any> {
         return this.http.put(this.getURL(this._links.relation.href), resource._links.self.href, {headers: ResourceHelper.headers});
     }
 
-
     // Unbind the resource with the given relation from this resource
-
-    public unbind(relation: string): Observable<any> {
+    public deleteRelation(relation: string): Observable<any> {
         return this.http.delete(this.getURL(this._links.relation.href), {headers: ResourceHelper.headers});
     }
 
     // Adds the given resource to the bound collection by the relation
-
-    public add<T extends Resource>(relation: string, resource: T): Observable<any> {
+    public addRelation<T extends Resource>(relation: string, resource: T): Observable<any> {
         let header = ResourceHelper.headers.append('Content-Type', 'text/uri-list')
         return this.http.post(this.getURL(this._links[relation].href), resource._links.self.href, {headers: header});
     }
--- src/rest.service.ts
+++ src/rest.service.ts
@@ -1,4 +1,4 @@
-import {Observable} from "rxjs/Observable";
+import {Observable} from 'rxjs/Observable';
 import {Resource} from './resource';
 import {ResourceArray} from './resource-array';
 import {Sort} from './sort';
@@ -6,91 +6,108 @@ import {Injector} from '@angular/core';
 import {ResourceService} from './resource.service';
 
 export class RestService<T extends Resource> {
-  private type: any;
-  private resource: string;
-  public resourceArray: ResourceArray<T>;
-  private resourceService: ResourceService
+    private type: any;
+    private resource: string;
+    public resourceArray: ResourceArray<T>;
+    private resourceService: ResourceService;
 
-  constructor(type: { new(): T }, resource: string, private injector: Injector) {
+    constructor(type: { new(): T }, resource: string, private injector: Injector) {
         this.type = type;
         this.resource = resource;
         this.resourceService = injector.get(ResourceService);
     }
 
-  protected handleError(error: any) {
+    public getAll(): Observable<T[]> {
+        return this.resourceService.getAll(this.type, this.resource)
+            .map((resourceArray: ResourceArray<T>) => {
+                this.resourceArray = resourceArray;
+                return resourceArray.result;
+            });
+    }
 
-    return Observable.throw(error);
-  }
+    public get(id: any): Observable<T> {
+        return this.resourceService.get(this.type, this.resource, id);
+    }
 
-  public getAll(): Observable<T[]> {
-    return this.resourceService.getAll(this.type, this.resource)
-      .map((resourceArray: ResourceArray<T>) => {
-        this.resourceArray = resourceArray;
-        return resourceArray.result;
-      });
-  }
+    public search(query: string, options?: {
+        size?: number, sort?: Sort[], params?: [{ key: string, value: string | number }]
+    }): Observable<T[]> {
+        return this.resourceService.search(this.type, query, this.resource, options)
+            .map((resourceArray: ResourceArray<T>) => {
+                this.resourceArray = resourceArray;
+                return resourceArray.result;
+            });
+    }
 
-  public get(id: any): Observable<T> {
-    return this.resourceService.get(this.type, this.resource, id);
-  }
+    public create(entity: T): Observable<Object> {
+        return this.resourceService.create(entity);
+    }
 
-  public search(query: string, options?: {
-    size?: number, sort?: Sort[], params?: [{ key: string, value: string | number }]
-  }): Observable<T[]> {
-    return this.resourceService.search(this.type, query, this.resource, options)
-      .map((resourceArray: ResourceArray<T>) => {
-        this.resourceArray = resourceArray;
-        return resourceArray.result;
-      });
-  }
+    public update(entity: T): Observable<Object> {
+        return this.resourceService.update(entity);
+    }
 
-  public hasNext(): boolean {
-    if (this.resourceArray)
-      return this.resourceService.hasNext(this.resourceArray);
-    return false;
-  }
+    public patch(entity: T): Observable<Object> {
+        return this.resourceService.patch(entity);
+    }
 
-  public next(): Observable<T[]> {
-    if (this.resourceArray)
-      return this.resourceService.next(this.resourceArray, this.type)
-        .map((resourceArray: ResourceArray<T>) => {
-          this.resourceArray = resourceArray;
-          return resourceArray.result;
-        });
-    else
-      Observable.throw("no resourceArray found");
-  }
+    public delete(entity: T): Observable<Object> {
+        return this.resourceService.delete(entity);
+    }
 
-  public prev(): Observable<T[]> {
-    if (this.resourceArray)
-      return this.resourceService.prev(this.resourceArray, this.type)
-        .map((resourceArray: ResourceArray<T>) => {
-          this.resourceArray = resourceArray;
-          return resourceArray.result;
-        });
-    else
-      Observable.throw("no resourceArray found");
-  }
+    public hasNext(): boolean {
+        if (this.resourceArray)
+            return this.resourceService.hasNext(this.resourceArray);
+        return false;
+    }
 
-  public first(): Observable<T[]> {
-    if (this.resourceArray)
-      return this.resourceService.first(this.resourceArray, this.type)
-        .map((resourceArray: ResourceArray<T>) => {
-          this.resourceArray = resourceArray;
-          return resourceArray.result;
-        });
-    else
-      Observable.throw("no resourceArray found");
-  }
+    public hasPrev(): boolean {
+        if (this.resourceArray)
+            return this.resourceService.hasPrev(this.resourceArray);
+        return false;
+    }
 
-  public last(): Observable<T[]> {
-    if (this.resourceArray)
-      return this.resourceService.last(this.resourceArray, this.type)
-        .map((resourceArray: ResourceArray<T>) => {
-          this.resourceArray = resourceArray;
-          return resourceArray.result;
-        });
-    else
-      Observable.throw("no resourceArray found");
-  }
+    public next(): Observable<T[]> {
+        if (this.resourceArray)
+            return this.resourceService.next(this.resourceArray, this.type)
+                .map((resourceArray: ResourceArray<T>) => {
+                    this.resourceArray = resourceArray;
+                    return resourceArray.result;
+                });
+        else
+            Observable.throw('no resourceArray found');
+    }
+
+    public prev(): Observable<T[]> {
+        if (this.resourceArray)
+            return this.resourceService.prev(this.resourceArray, this.type)
+                .map((resourceArray: ResourceArray<T>) => {
+                    this.resourceArray = resourceArray;
+                    return resourceArray.result;
+                });
+        else
+            Observable.throw('no resourceArray found');
+    }
+
+    public first(): Observable<T[]> {
+        if (this.resourceArray)
+            return this.resourceService.first(this.resourceArray, this.type)
+                .map((resourceArray: ResourceArray<T>) => {
+                    this.resourceArray = resourceArray;
+                    return resourceArray.result;
+                });
+        else
+            Observable.throw('no resourceArray found');
+    }
+
+    public last(): Observable<T[]> {
+        if (this.resourceArray)
+            return this.resourceService.last(this.resourceArray, this.type)
+                .map((resourceArray: ResourceArray<T>) => {
+                    this.resourceArray = resourceArray;
+                    return resourceArray.result;
+                });
+        else
+            Observable.throw('no resourceArray found');
+    }
 }

d20cd4e2ef6994a4cb75ca898c0161c51883bc41
sante.stanisci@masvis.com <sante.stanisci@masvis.com>
2018-01-15 13:02:54 +0100

    fix providers

--- index.ts
+++ index.ts
@@ -2,19 +2,20 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {RestService} from './src/rest.service';
 import {ExternalService} from './src/external.service';
+import {ResourceService} from './src/resource.service';
 
-export {API_URI, PROXY_URI, ExternalService} from "./src/external.service";
+export {API_URI, PROXY_URI, ExternalService} from './src/external.service';
 export {RestService} from './src/rest.service';
-export {Resource} from "./src/resource";
-export {ResourceArray} from "./src/resource-array";
-export {Sort} from "./src/sort";
-export {ResourceHelper} from "./src/resource-helper";
+export {Resource} from './src/resource';
+export {ResourceArray} from './src/resource-array';
+export {Sort} from './src/sort';
+export {ResourceHelper} from './src/resource-helper';
 
 @NgModule({
   imports: [HttpClientModule],
   declarations: [],
   exports: [HttpClientModule],
-  providers: [HttpClient]
+  providers: [HttpClient, ResourceService]
 })
 export class AngularHalModule {
   static forRoot(): ModuleWithProviders {

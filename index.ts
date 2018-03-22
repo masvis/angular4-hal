893408ab3e724cae21f8e530a3d4316787221802
Sante Stanisci <sante.stanisci@hotmail.it>
2018-03-22 13:11:54 +0100

    gestione dell'url da servizio esterno

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {RestService} from './src/rest.service';
 import {ExternalService} from './src/external.service';
 import {ResourceService} from './src/resource.service';
+import {ExternalConfigurationHandlerInterface} from './src/external-configuration.handler';
 
 export {ExternalService} from './src/external.service';
 export {RestService} from './src/rest.service';
@@ -10,6 +11,8 @@ export {Resource} from './src/resource';
 export {ResourceArray} from './src/resource-array';
 export {Sort} from './src/sort';
 export {ResourceHelper} from './src/resource-helper';
+export {ExternalConfiguration} from './src/ExternalConfiguration';
+export {ExternalConfigurationHandlerInterface} from './src/external-configuration.handler';
 
 @NgModule({
   imports: [HttpClientModule],

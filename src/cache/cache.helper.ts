5b16693a6f25adae4ca9e4a8d48ce7363d170439
Sante Stanisci <sante.stanisci@hotmail.it>
2019-01-17 14:58:23 +0100

    cache with hash key

--- src/cache/cache.helper.ts
+++ src/cache/cache.helper.ts
@@ -1,6 +1,8 @@
 import {Resource} from '../resource';
+import * as hash from 'hash.js';
+import {HalOptions} from '../rest.service';
 
-export type ResourceExpire<T extends Resource> = { entity: any, expire: number };
+export type ResourceExpire<T extends Resource> = { entity: any, body?: any, params?: HalOptions, expire: number };
 
 export enum EvictStrategy {
     EvictTrivial,
@@ -11,6 +13,8 @@ export class CacheHelper {
     private static cacheMap: Map<string, ResourceExpire<any>> = new Map<string, ResourceExpire<any>>();
 
     static isActive = true;
+    // TODO
+    static maxEntries: number = 100;
     static evictStrategy: EvictStrategy = EvictStrategy.EvictTrivial;
     static defaultExpire: number = 10 * 60 * 1000; //10 minutes
 
@@ -23,38 +27,38 @@ export class CacheHelper {
                 else if (CacheHelper.evictStrategy == EvictStrategy.EvictSmart) {
                     this.cacheMap.forEach((value: ResourceExpire<any>, key: string) => {
                         if (value.expire > 0 && Date.now() > value.expire)
-                            this.cacheMap.delete(key);
+                            this.evict(key);
                     });
                 }
             }, 15 * 60 * 1000);
         }
     }
 
-    static ifPresent<T extends Resource>(link: string): boolean {
+    static ifPresent<T extends Resource>(link: string, body?: string, params?: HalOptions): boolean {
         if (!this.isActive)
             return false;
-        return this.cacheMap.has(link);
+        return this.cacheMap.has(CacheHelper.key(link, body, params));
     }
 
-    static getArray<T extends Resource>(link: string): T[] {
-        return this.cacheMap.get(link).entity;
+    static getArray<T extends Resource>(link: string, body?: string, params?: HalOptions): T[] {
+        return this.cacheMap.get(CacheHelper.key(link, body, params)).entity;
     }
 
-    static putArray<T extends Resource>(link: string, array: T[], expireMs: number = 10 * 60 * 1000) {
+    static putArray<T extends Resource>(link: string, array: T[], expireMs: number = 10 * 60 * 1000, body?: string, params?: HalOptions) {
         if (this.isActive) {
             let resourceExpire: ResourceExpire<T> = {entity: array, expire: CacheHelper.expireDate(expireMs)};
-            this.cacheMap.set(link, resourceExpire);
+            this.cacheMap.set(CacheHelper.key(link, body, params), resourceExpire);
         }
     }
 
-    static get<T extends Resource>(link: string): T {
-        return this.cacheMap.get(link).entity;
+    static get<T extends Resource>(link: string, body?: string, params?: HalOptions): T {
+        return this.cacheMap.get(CacheHelper.key(link, body, params)).entity;
     }
 
-    static put<T extends Resource>(link: string, array: T, expireMs: number = 10 * 60 * 1000) {
+    static put<T extends Resource>(link: string, array: T, expireMs: number = 10 * 60 * 1000, body?: string, params?: HalOptions) {
         if (this.isActive) {
             let resourceExpire: ResourceExpire<T> = {entity: array, expire: CacheHelper.expireDate(expireMs)};
-            this.cacheMap.set(link, resourceExpire);
+            this.cacheMap.set(CacheHelper.key(link, body, params), resourceExpire);
         }
     }
 
@@ -64,8 +68,43 @@ export class CacheHelper {
         return Date.now() + expireMs;
     }
 
-    static evict(link: string) {
-        this.cacheMap.delete(link);
+    private static key(link: string, body?: string, params?: HalOptions): string {
+
+        let k: string = link;
+        if (body)
+            k += body;
+
+        if (params)
+            k += CacheHelper.toStringParams(params);
+
+        let key: string = hash.sha256().update(k).digest('hex');
+        console.log(key);
+        return key;
+    }
+
+    private static toStringParams(options: HalOptions) {
+        let s: string = '';
+        if (options.size) {
+            s = 'size' + options.size.toString();
+        }
+
+        if (options.notPaged) {
+            s += 'notPaged';
+        }
+
+        if (options.sort) {
+            for (const sortInfo of options.sort) {
+                let sortString = '';
+                sortString = sortInfo.path ? sortString.concat(sortInfo.path) : sortString;
+                sortString = sortInfo.order ? sortString.concat(',').concat(sortInfo.order) : sortString;
+                s += 'sort' + sortString;
+            }
+        }
+        return s;
+    }
+
+    static evict(key: string) {
+        this.cacheMap.delete(key);
     }
 
     static evictAll() {

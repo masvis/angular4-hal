15e5827afac95fb1b4967a8ef25c1ce919847f33
sante stanisci <s.sante@hotmail.it>
2020-02-22 16:01:36 +0100

    fix interceptor

--- src/interceptor/AuthInterceptor.ts
+++ src/interceptor/AuthInterceptor.ts
@@ -1,21 +1,18 @@
 import {Inject, Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest, HttpResponse} from "@angular/common/http";
+import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {TokenConfigService} from "./TokenConfigService";
 import {Observable} from "rxjs";
 import {Auth} from "./Auth";
-import {tap} from "rxjs/operators";
-import {Router} from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(@Inject(TokenConfigService) private config, private router: Router) {
-    }
+    constructor(@Inject(TokenConfigService) private config) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
-        if (this.config) {
-            const token: Auth = Object.assign(Auth, JSON.parse(sessionStorage.getItem(this.config.token)));
+        if(this.config) {
+            const token: Auth = Object.assign(Auth , JSON.parse(sessionStorage.getItem(this.config.token)));
 
             if (token && request.url.indexOf('files/upload') != -1) {
                 const headers = new HttpHeaders({
@@ -33,17 +30,6 @@ export class AuthInterceptor implements HttpInterceptor {
             }
         }
 
-        return next.handle(request).pipe(
-            tap(evt => {
-                if (evt instanceof HttpResponse) {
-                    if (evt.status !== 401) {
-                        return;
-                    }
-                    if (this.config.loginPath)
-                        this.router.navigate([this.config.loginPath]);
-                }
-            }));
+        return next.handle(request);
     }
 }
-
-

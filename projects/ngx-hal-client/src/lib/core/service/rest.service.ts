3016984af22274d0ab4e40b424d315137d5e458e
ilya <ilya@lagoshny.ru>
2020-06-20 12:16:31 +0300

    Added getAllPage method to the rest.service.ts

--- projects/ngx-hal-client/src/lib/core/service/rest.service.ts
+++ projects/ngx-hal-client/src/lib/core/service/rest.service.ts
@@ -48,6 +48,15 @@ export class RestService<T extends Resource> {
             }));
     }
 
+    public getAllPage(options?: HalOptions, subType?: SubTypeBuilder): Observable<ResourcePage<T>> {
+        return this.resourceService.getAll(this.type, this.resource, this.embedded, options, subType)
+            .pipe(
+                mergeMap((resourceArray: ResourceArray<T>) => {
+                    return observableOf(new ResourcePage<T>(resourceArray));
+                })
+            );
+    }
+
     public get(id: any, params?: HalParam[]): Observable<T> {
         return this.resourceService.get(this.type, this.resource, id, params);
     }
